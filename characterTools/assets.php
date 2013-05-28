12b85119200116b8c53539b61dc9626232841b5b
Aaron Aichlmayr <waterfoul@gmail.com>
2013-05-27 23:30:59 -0500

    finishing the asset loader

--- characterTools/assets.php
+++ characterTools/assets.php
@@ -69,7 +69,8 @@
 		                    SELECT a.flag, a.lvl, a.lft, a.rgt, a.quantity, i.typeName, i.description, a.locationID, a.itemID
 		                    FROM  charAssetList as a 
 							JOIN `naa_dbdump`.`invTypes` as i ON i.`typeID` = a.`typeID`
-		                    WHERE a.ownerID = " . $currentChar . " AND lft > " . $lft . " AND rgt < " . $rgt . " AND a.lvl = " . $level
+		                    WHERE a.ownerID = " . $currentChar . " AND lft > " . $lft . " AND rgt < " . $rgt . " AND a.lvl = " . $level . "
+                            ORDER BY a.locationID"
 		                );
 						while($assetRow = $assetQry->fetch_object())
 						{
@@ -81,7 +82,7 @@
 						}
 						return $retval;
 					}
-					function print_rows($result, $class)
+					function print_rows($result, $class, $phpBB)
 					{
 						$css = "";
 						if(!empty($class))
@@ -91,17 +92,87 @@
 							$lvl = "";
 							for($i=1;$i<$v->lvl;$i++)
 								$lvl .= ">";
+							$flag = get_flag($v->flag, $phpBB);
 							print("<tr onclick='$(\"." . $v->itemID . "\").toggle()' class='" . $class . "' style='" . $css . "'>
 								<td>" . $lvl . "</td>
-								<td>" . $v->flag . "</td>
+								<td class='descr'>" . $flag[0] . "<div>" . $flag[1] . "</div></td>
 								<td>" . $v->quantity . "</td>
 								<td class='descr'>" . $v->typeName . "<div>" . nl2br($v->description) . "</div></td>
-								<td>" . $v->locationID . "</td>
+								<td>" . get_station($v->locationID, $phpBB) . "</td>
 								<td>" . count($v->contents) . "</td>
 							</tr>");
 							if(!empty($v->contents))
-								print_rows($v->contents, $v->itemID);
+								print_rows($v->contents, $v->itemID, $phpBB);
 						}
+					}
+					function get_flag($id, $phpBB)
+					{
+						static $flags = array();
+						if(empty($flags[$id]))
+						{
+							$qry = $phpBB->query("SELECT s.flagName, s.flagText FROM naa_dbdump.invFlags AS s WHERE s.flagID=" . $id);
+							if($row=$qry->fetch_object())
+								$flags[$id] = Array($row->flagName, $row->flagText);
+							else
+								$flags[$id] = Array("Unknown", $id);
+						}
+						return $flags[$id];
+					}
+					function get_station($id, $phpBB)
+					{
+						static $stations = array();
+						if(empty($stations[$id]))
+						{
+							if(66000000 < $id && $id > 66014933)
+							{
+								$qry = $phpBB->query("SELECT s.stationName FROM naa_dbdump.staStations AS s WHERE s.stationID=" . ($id-6000001));
+								if($row=$qry->fetch_object())
+									$stations[$id] = $row->stationName;
+								else
+									$stations[$id] = "Unknown " . $id;
+							}
+							else if(66014934 < $id && $id > 67999999)
+							{
+								$qry = $phpBB->query("SELECT c.stationName FROM naa_yapeal.eveConquerableStationList AS c WHERE c.stationID=" . ($id-6000000));
+								if($row=$qry->fetch_object())
+									$stations[$id] = $row->stationName;
+								else
+									$stations[$id] = "Unknown " . $id;
+							}
+							else if(60014861 < $id && $id > 60014928)
+							{
+								$qry = $phpBB->query("SELECT c.stationName FROM naa_yapeal.eveConquerableStationList AS c WHERE c.stationID=" . ($id));
+								if($row=$qry->fetch_object())
+									$stations[$id] = $row->stationName;
+								else
+									$stations[$id] = "Unknown " . $id;
+							}
+							else if(60000000 < $id && $id > 61000000)
+							{
+								$qry = $phpBB->query("SELECT s.stationName FROM naa_dbdump.staStations AS s WHERE s.stationID=" . ($id));
+								if($row=$qry->fetch_object())
+									$stations[$id] = $row->stationName;
+								else
+									$stations[$id] = "Unknown " . $id;
+							}
+							else if($id >= 61000000)
+							{
+								$qry = $phpBB->query("SELECT c.stationName FROM naa_yapeal.eveConquerableStationList AS c WHERE c.stationID=" . ($id));
+								if($row=$qry->fetch_object())
+									$stations[$id] = $row->stationName;
+								else
+									$stations[$id] = "Unknown " . $id;
+							}
+							else
+							{
+								$qry = $phpBB->query("SELECT m.itemName FROM naa_dbdump.mapDenormalize AS m WHERE m.itemID=" . ($id));
+								if($row=$qry->fetch_object())
+									$stations[$id] = $row->itemName;
+								else
+									$stations[$id] = "Unknown " . $id;
+							}
+						}
+						return $stations[$id];
 					}
 	                $corpseQry = $yapeal->query("
 	                    SELECT a.rgt, a.lft
@@ -113,7 +184,7 @@
 						$result = getAssets($corpseRow->lft, $corpseRow->rgt, 1, $currentChar, $yapeal);
 						print("<table>");
 						print("<thead><tr><th>&nbsp;&nbsp;&nbsp;&nbsp;</th><th>Flag</th><th>#</th><th>Name</th><th>Station</th><th>Amount Held</th></tr></thead><tbody>");
-						print_rows($result, "");
+						print_rows($result, "", $phpBB);
 						print("</tbody></table>");
 					}
 				}
