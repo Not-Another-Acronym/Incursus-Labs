1f263d2ca73bfea1446fefd8127a1d3e3cc5b157
waterfoul <waterfoul@NAA.waterfoul.net>
2013-08-19 04:19:48 +0400

    Moved the wiki db settings out of the localsettings file

---
+++ w/wikiDB.php
@@ -0,0 +1,9 @@
+<?php
+	$wgDBname           = "warptome_wiki";
+	$wgDBuser           = "warptome_incurs";
+	$wgDBpassword       = "OP-{ty)h5^22";
+	
+	$wgAuth_Config['MySQL_Username']    = $wgDBuser;       // phpBB MySQL Username.
+	$wgAuth_Config['MySQL_Password']    = $wgDBpassword;       // phpBB MySQL Password.
+	$wgAuth_Config['MySQL_Database']    = 'warptome_phpBB';       // phpBB MySQL Database Name.
+?>
